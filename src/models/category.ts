73805885863df1a9f27d1bb07bf6e524e5ddd2d9
Vladyslav Ivashko <vladyslavivashko@MacBook-Air.local>
2025-08-02 01:49:58 +0300

    feat: add mock data

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,7 +1,68 @@
 import { Category } from '../types/category'
 import { CreateCategory, UpdateCategory } from '../utils/category'
 
-const categories: Category[] = []
+const categories: Category[] = [
+    {
+    id: 'c1',
+    name: 'Frontend',
+    createdAt: '2025-06-01T09:00:00Z',
+    updatedAt: '2025-06-01T09:00:00Z',
+  },
+  {
+    id: 'c2',
+    name: 'Backend',
+    createdAt: '2025-06-02T10:00:00Z',
+    updatedAt: '2025-06-02T10:00:00Z',
+  },
+  {
+    id: 'c3',
+    name: 'DevOps',
+    createdAt: '2025-06-03T11:00:00Z',
+    updatedAt: '2025-06-03T11:00:00Z',
+  },
+  {
+    id: 'c4',
+    name: 'UX/UI',
+    createdAt: '2025-06-04T12:00:00Z',
+    updatedAt: '2025-06-04T12:00:00Z',
+  },
+  {
+    id: 'c5',
+    name: 'Testing',
+    createdAt: '2025-06-05T13:00:00Z',
+    updatedAt: '2025-06-05T13:00:00Z',
+  },
+  {
+    id: 'c6',
+    name: 'Security',
+    createdAt: '2025-06-06T14:00:00Z',
+    updatedAt: '2025-06-06T14:00:00Z',
+  },
+  {
+    id: 'c7',
+    name: 'Maintenance',
+    createdAt: '2025-06-07T15:00:00Z',
+    updatedAt: '2025-06-07T15:00:00Z',
+  },
+  {
+    id: 'c8',
+    name: 'Analytics',
+    createdAt: '2025-06-08T16:00:00Z',
+    updatedAt: '2025-06-08T16:00:00Z',
+  },
+  {
+    id: 'c9',
+    name: 'Project Management',
+    createdAt: '2025-06-09T17:00:00Z',
+    updatedAt: '2025-06-09T17:00:00Z',
+  },
+  {
+    id: 'c10',
+    name: 'Research',
+    createdAt: '2025-06-10T18:00:00Z',
+    updatedAt: '2025-06-10T18:00:00Z',
+  },
+]
 
 export const CategoryModel = {
   findAll: () => categories,
