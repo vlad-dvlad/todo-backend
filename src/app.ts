7f4aac7cd0f11f751ee5849263cfb08e3eb185a9
Vladyslav Ivashko <vladyslavivashko@MacBook-Air.local>
2025-08-05 01:38:58 +0300

    feat: cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import task from './routes/task'
 import user from './routes/user'
 import { errorMiddleware } from './middlewares/error'
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+}
+
 const app = express()
 app.use(express.json())
+app.use(cors(corsOptions))
 
 app.use('/tasks', task)
 app.use('/users', user)
